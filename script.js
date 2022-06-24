481cdc676e3cf1be7d6f4918e2cbda25dde21ffb
IDDna <romanov1371@gmail.com>
2022-06-24 10:52:02 +0300

    Create playRound function

--- script.js
+++ script.js
@@ -1,5 +1,21 @@
 function computerPlay() {
    let computerChoice = Math.floor(Math.random() * 3);
-   return (computerChoice === 0) ? 'Rock' :
-      (computerChoice === 1) ? 'Paper' : 'Scissors';
+   return (computerChoice === 0) ? 'rock' :
+      (computerChoice === 1) ? 'paper' : 'scissors';
+}
+
+function playRound(playerSelection, computerSelection) {
+   if (playerSelection.toLowerCase() === 'rock') {
+      if (computerSelection === 'rock') return 'Tie! Computer showed the same symbol';
+      else if (computerSelection === 'paper') return 'You lost! Rock doesn\'t beat Paper';
+      else return 'You won! Rock beats Scissors'
+   } else if (playerSelection.toLowerCase() === 'paper') {
+      if (computerSelection === 'paper') return 'Tie! Computer showed the same symbol';
+      else if (computerSelection === 'scissors') return 'You lost! Paper doesn\'t beat Scissors';
+      else return 'You won! Paper beats Rock'
+   } else {
+      if (computerSelection === 'scissors') return 'Tie! Computer showed the same symbol';
+      else if (computerSelection === 'rock') return 'You lost! Scissors don\'t beat Rock';
+      else return 'You won! Scissors beat Paper'
+   } 
 }
