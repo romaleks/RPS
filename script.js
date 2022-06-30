0dba1c8a21b3bd927d6ba074cd1b3b62c3c9c9cc
IDDna <romanov1371@gmail.com>
2022-06-30 16:55:21 +0300

    Rewrite playRound function

--- script.js
+++ script.js
@@ -5,20 +5,21 @@ function computerPlay() {
 }
 
 function playRound(playerSelection, computerSelection) {
-   if (playerSelection.toLowerCase() === 'rock') {
-      if (computerSelection === 'Rock') return 'Tie! Computer showed the same symbol';
-      else if (computerSelection === 'Paper') return 'You lost! Rock doesn\'t beat Paper';
-      else return 'You won! Rock beats Scissors'
-   } else if (playerSelection.toLowerCase() === 'paper') {
-      if (computerSelection === 'Paper') return 'Tie! Computer showed the same symbol';
-      else if (computerSelection === 'Scissors') return 'You lost! Paper doesn\'t beat Scissors';
-      else return 'You won! Paper beats Rock'
+   if (playerSelection === 'rock') {
+      if (computerSelection === 'rock') return ['Tie!', 'Computer showed the same symbol'];
+      else if (computerSelection === 'paper') return ['You lost!', 'Rock doesn\'t beat Paper'];
+      else return ['You won!', 'Rock beats Scissors'];
+   } else if (playerSelection === 'paper') {
+      if (computerSelection === 'paper') return ['Tie!', 'Computer showed the same symbol'];
+      else if (computerSelection === 'scissors') return ['You lost!', 'Paper doesn\'t beat Scissors'];
+      else return ['You won!', 'Paper beats Rock'];
    } else {
-      if (computerSelection === 'Scissors') return 'Tie! Computer showed the same symbol';
-      else if (computerSelection === 'Rock') return 'You lost! Scissors don\'t beat Rock';
-      else return 'You won! Scissors beat Paper'
+      if (computerSelection === 'scissors') return ['Tie!', 'Computer showed the same symbol'];
+      else if (computerSelection === 'rock') return ['You lost!', 'Scissors don\'t beat Rock'];
+      else return ['You won!', 'Scissors beat Paper'];
    } 
 }
+
 
 // function game() {
 
